9cd58a91b95e5217d1a173724c6c898211581bc9
tanmaysainighy <tanmay.sainighy@gmail.com>
2026-09-06 12:27:45 +0530

    Disable API response caching so fresh TinyFish results are always returned

--- api/events.js
+++ api/events.js
@@ -174,6 +174,9 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
 
   if (req.method === 'OPTIONS') {
     res.status(200).end();
